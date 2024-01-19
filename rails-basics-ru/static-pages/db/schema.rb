b57324cc37ef45ff83b862881a5fbc12c008397a
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2024-01-19 23:17:50 +1300

    Cheat with the migration version

--- rails-basics-ru/static-pages/db/schema.rb
+++ rails-basics-ru/static-pages/db/schema.rb
@@ -10,5 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_09_19_122050) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_122050) do
 end
